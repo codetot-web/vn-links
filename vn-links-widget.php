8118de4a1e0c8fc42042e8df4bf5f74f2013728d
CODE TOT JSC <dev@codetot.com>
2021-11-17 16:16:53 +0700

    Init project

---
+++ vn-links-widget.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link              https://codetot.com
+ * @since             0.0.1
+ * @package           Vn_Links_Widget
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Vietnam Links Widget
+ * Plugin URI:        https://codetot.com
+ * Description:       Manage external links and display links as dropdown in a single widget. One of most feature for government sites in Vietnam.
+ * Version:           0.0.1
+ * Author:            CODE TOT JSC
+ * Author URI:        https://codetot.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       vn-links-widget
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'VN_LINKS_WIDGET_VERSION', '5.0.1' );
+define( 'VN_LINKS_WIDGET_DIR', plugin_dir_path(__FILE__));
+define( 'VN_LINKS_WIDGET_URI', plugins_url('ct-blocks'));
